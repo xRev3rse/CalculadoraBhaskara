32ae4ee28ca7f7ddff385c1db325dca35ea3f29e
xRev3rse <96670789+xRev3rse@users.noreply.github.com>
2022-05-06 21:44:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,39 +4,117 @@ function calcular() {
     var valorC = document.querySelector('#valorC').value;
     var res    = document.querySelector('#res');
     
-    valorA = Number(valorA)
-    valorB = Number(valorB)
-    valorC = Number(valorC)
-
-    AxC        = valorA * valorC 
-    Bneg       = -valorB
-    BmA        = valorA + Bneg
-    DxA        = 2 * valorA
-    BmenosA    = Bneg - valorA
-    BmenosA_div= BmenosA / DxA
-    four_axc   = 4 * AxC
-    bvalue     = valorB * valorB
-    deltavalue = bvalue - four_axc
-    BmA_div    = BmA / DxA
-    raiz_delta = Math.sqrt(deltavalue)
+    a = Number(valorA);
+    b = Number(valorB);
+    c = Number(valorC);
     
-    res.innerHTML =  'Δ = b² - 4.a.c <br>'
-    res.innerHTML += `Δ = ${valorB}² - 4.${valorA}.${valorC} <br>`
-    res.innerHTML += `Δ = ${valorB}² - 4.${AxC} <br>`
-    res.innerHTML += `Δ = ${valorB}² - ${four_axc}<br>`
-    res.innerHTML += `Δ = ${bvalue} - ${four_axc}<br>`
-    res.innerHTML += `Δ = ${deltavalue} <br><br><hr><br>`
+    var delta     = b ** 2 - (4 * a * c);
+    var r1        = (-b + Math.sqrt(delta)) / (2 * a);
+    var r2        = (-b - Math.sqrt(delta)) / (2 * a);
+    var r         = (-b) / (2 * a);
+    var raizdelta = Math.sqrt(delta);
+    console.log(r1, r2)
+    
 
-    res.innerHTML += `x = -B ± √Δ ÷ 2.a<br>`
-    res.innerHTML += `x = -${valorB} ± √${deltavalue} ÷ 2.${valorA}<br>`
-    res.innerHTML += `x = -${valorB} ± ${raiz_delta} ÷ 2.${valorA}<br>`
-    res.innerHTML += `x = -${valorB} ± ${raiz_delta} ÷ ${DxA}<br><br><hr><br>`
+    
+    if (delta < 0) {
+        res.innerHTML = 'Não há raízes reais (Delta menor que zero).';
+    } else if (delta > 0) {
+        res.innerHTML =  'Calculando o delta... &#128516<br>';
+        res.innerHTML +=  'Δ = b² - 4.a.c <br>';
+        res.innerHTML +=  `Δ = ${b}² - 4.${a}.${c} <br>`;
+        res.innerHTML += `Δ = ${b}² - 4.${a * c} <br>`;
+        res.innerHTML += `Δ = ${b ** 2} - ${4 * a * c} <br>`;
+        res.innerHTML += `Δ = ${delta} <br><br><hr><br>`;
+        
 
-    res.innerHTML += `XI = -${valorB} + ${valorA} ÷ ${DxA}<br>`
-    res.innerHTML += `XI = ${BmA} ÷ ${DxA}<br>`
-    res.innerHTML += `XI = ${BmA_div}<br><br>&<br><br>`
 
-    res.innerHTML += `XII = -${valorB} - ${valorA} ÷ ${DxA}<br>`
-    res.innerHTML += `XII = ${BmenosA} ÷ ${DxA}<br>`
-    res.innerHTML += `XII = ${BmenosA_div}`
+        // Se B for menor que zero...
+        if (b < 0) {
+            b = b * -1;
+            if (Number.isInteger(raizdelta)) {
+                res.innerHTML += `x = -b ± √Δ ÷ 2·a<br>`
+                res.innerHTML += `x = -(-${b}) ± √${delta} ÷ 2·${a}<br>`
+                res.innerHTML += `x = ${b} ± ${raizdelta} ÷ ${2 * a}<br><br>`
+
+                res.innerHTML += `x1 e x2 encontrados!&#128513;<br>`
+               
+                res.innerHTML += `x1 = ${b} + ${raizdelta} ÷ ${2 * a}<br>`
+                res.innerHTML += `x1 = ${b + raizdelta} ÷ ${2 * a}<br>`
+                res.innerHTML += `x1 = ${r1}<br><br>`
+                
+                res.innerHTML += `x2 = ${b} - ${raizdelta} ÷ ${2 * a}<br>`
+                res.innerHTML += `x2 = ${b - raizdelta} ÷ ${2 * a}<br>`
+                res.innerHTML += `x2 = ${r2}<br>`
+            } else {
+                res.innerHTML += `Informação: O delta não possui raiz quadrada<br>`
+                res.innerHTML += `x = –b ± √Δ ÷ 2·a<br>`
+                res.innerHTML += `x = -(-${b}) ± √${delta} ÷ 2·${a}<br>`
+                res.innerHTML += `x = ${b/(2*a)} ± √${delta} ÷ ${2 * a}<br><br>`
+
+                res.innerHTML += `x1 e x2 encontrados!&#128513;<br>`
+                res.innerHTML += `x1 = ${b/(2*a)} + √${delta}<br>`
+                res.innerHTML += `x2 = ${b/(2*a)} - √${delta}<br>`
+            }
+
+
+
+        // Se B for maior que zero...
+        } else if (b > 0){
+            b = b * -1;
+            if (Number.isInteger(raizdelta)) {
+                res.innerHTML += `x = –b ± √Δ ÷ 2·a<br>`
+                res.innerHTML += `x = ${b} ± √${delta} ÷ 2·${a}<br>`
+                res.innerHTML += `x = ${b} ± √${raizdelta} ÷ ${2 * a}<br><br><hr><br>`
+
+                res.innerHTML += `x1 e x2 encontrados!&#128513;<br>`
+                res.innerHTML += `x1 = ${b} + ${raizdelta} ÷ ${2 * a}<br>`
+                res.innerHTML += `x1 = ${b + raizdelta} ÷ ${2 * a}<br>`
+                res.innerHTML += `x1 = ${r1}<br><br>`
+
+                res.innerHTML += `x2 = ${b} - ${raizdelta} ÷ ${2 * a}<br>`
+                res.innerHTML += `x2 = ${b - raizdelta} ÷ ${2 * a}<br>`
+                res.innerHTML += `x2 = ${r2}<br><br>`
+            } else {
+                res.innerHTML += `Informação: O delta não possui raiz quadrada<br>`
+                res.innerHTML += `x = –b ± √Δ ÷ 2·a<br>`
+                res.innerHTML += `x = ${b} ± √${delta} ÷ 2·${a}<br>`
+                res.innerHTML += `x = ${b} ± √${delta} ÷ ${2 * a}<br><br>`
+
+                res.innerHTML += `x1 e x2 encontrados!&#128513;<br>`
+                res.innerHTML += `x1 = ${b/(2*a)} + √${delta}<br>`
+                res.innerHTML += `x2 = ${b/(2*a)} - √${delta}<br>`
+
+
+
+            }
+        }
+    } else if (delta == 0) {
+        res.innerHTML =  'Calculando o delta... &#128516<br>'
+        res.innerHTML +=  'Δ = b² - 4.a.c <br>'
+        res.innerHTML +=  `Δ = ${b}² - 4.${a}.${c} <br>`
+        res.innerHTML += `Δ = ${b}² - 4.${a * c} <br>`
+        res.innerHTML += `Δ = ${b ** 2} - ${4 * a * c} <br>`
+        res.innerHTML += `Δ = ${delta} <br><br><hr><br>`
+        
+        b = b * -1
+        if (b > 0) {
+            res.innerHTML += `Raíz encontrada!&#128513;<br>`
+            res.innerHTML += `x = –b ± √Δ ÷ 2·a<br>`
+    
+            res.innerHTML += `x = ${b} ± √0 ÷ 2·${a}<br>`
+            res.innerHTML += `x = ${b} ± 0 ÷ ${2*a}<br>`
+            res.innerHTML += `x1 = ${b} + 0 ÷ ${2*a}<br>`
+            res.innerHTML += `A raíz da equação é: ${r}`
+        } else if (b < 0) {
+            res.innerHTML += `Raíz encontrada!&#128513;<br>`
+            res.innerHTML += `x = –b ± √Δ ÷ 2·a<br>`
+    
+            res.innerHTML += `x = -(${b}) ± √0 ÷ 2·${a}<br>`
+            res.innerHTML += `x = ${b} ± 0 ÷ ${2*a}<br>`
+            res.innerHTML += `x1 = ${b} + 0 ÷ ${2*a}<br>`
+            res.innerHTML += `A raíz da equação é: ${r}`
+        }
+    }
+    
 }
